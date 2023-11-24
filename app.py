4360d0c7f0f302655256cf05729900bf3afb1b6a
Jeffrey Ng <jeffreyng@Jeffreys-Mini.fios-router.home>
2023-11-24 05:54:04 -0500

    added homepage but still debugging

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, flash, redirect, session, g, sessions
 # from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
-
+import requests, json
+import json
+import pandas as pd
 # from forms import UserAddForm, LoginForm, MessageForm
 from models import db, connect_db
 
@@ -13,15 +15,30 @@ app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql:///jeffreyng'
 app.config['SQLALCHEMY_RECORD_QUERIES'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = "it's a secret"
 # toolbar = DebugToolbarExtension(app)
 
-
+api_key = '1288b79b'
 
 
 connect_db(app)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    df = pd.read_json('~/IMDB_IDs/IMDB.json')
+    url= 'http://www.omdbapi.com/?&i='
+    movies = df.movies
+    print(len(movies))
+    list_of_movies=[]
+    for x in movies[0:1]:
+        try:
+            resp = requests.get('http://www.omdbapi.com/?apikey=1288b79b&?i={}'.format(x))
+            print(resp.text)
+            list_of_movies.append(resp.text)
+        except:
+            pass
+    return render_template('home.html', list_of_movies = list_of_movies)
+
+
+
