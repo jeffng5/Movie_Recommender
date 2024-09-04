2cbd5684bc93b73b10761ed98c15cccd8876801b
jeffng5 <jeffrey.ng51213@outlook.com>
2024-09-04 07:40:33 -0400

    reconfigured environment

--- app.py
+++ app.py
@@ -18,27 +18,27 @@ from werkzeug.urls import url_encode
 DATABASE_URL = os.getenv("URL")
         
 app = Flask(__name__)
+connect_db(app)
+db.create_all()
+app.app_context().push()
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-# Get DB_URI from environ variable (useful for production/testing) or,
-# if not set there, use development local db.
+
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_RECORD_QUERIES'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 
     #opening the file
 with open('./embedding_many.pickle', 'rb') as f:
     embedding_many = pickle.load(f)
 
 
-connect_db(app)
 
-db.create_all()
-app.app_context().push()
 
 
 
