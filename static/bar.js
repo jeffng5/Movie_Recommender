5485ecb358dbcbcae913ada8dacdf7c48302f1cc
jeffng5 <jeffrey.ng51213@outlook.com>
2025-09-07 22:23:59 -0400

    troubleshooting progress bar

--- static/bar.js
+++ static/bar.js
@@ -11,9 +11,9 @@ function pollProgress() {
       fetch("/progress")
         .then(res => res.json())
         .then(data => {
-          document.getElementById("bar").style.width = data.percent + "%";
-          document.getElementById("percent").textContent = data.percent + "%";
-          if (data.percent >= 100) clearInterval(interval);
+          document.getElementById("bar").style.width = data.status + "%";
+          document.getElementById("percent").textContent = data.status + "%";
+          if (data.status >= 100) clearInterval(interval);
         });
     }, 500);
   }
